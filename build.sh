be87ec09966cd094210fbff64408b3ece3e405df
Lars Larsson <lars.la@gmail.com>
2015-11-21 19:18:44 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+[ -f /usr/bin/docker ] || echo "No docker binary available"; exit 1
+
+mkdir /tmp/grafana
+cd /tmp/grafana
+
+git clone https://github.com/grafana/grafana.git
+cd grafana/docker/production
+
+bash -x build.sh
+
+docker tag grafana/grafana:develop larsla/grafana-armv7l
